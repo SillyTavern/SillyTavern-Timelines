9ee1da40c364c052ba535bf79326d840f9b7ac49
city-unit <140349364+city-unit@users.noreply.github.com>
2023-08-04 01:41:35 -0400

    Use your own go.js, use your own popup. Functions+

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 // The tree should not be able to be edited, but should be able to be moved around
 
 
+var script = document.createElement('script');
+script.src = 'scripts/extensions/third-party/st-tree-extension/go.js';
+$('body').append(script);
 
 
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
@@ -124,8 +127,6 @@ function createNode(nodeKey, parentNodeKey, text, group) {
 	
 }
 
-
-
 // Update the last node for each chat in the group
 function updatePreviousNodes(previousNodes, nodeKey, group) {
 	group.forEach(({ file_name }) => {
@@ -146,7 +147,6 @@ function convertToGoJsTree(channelHistory) {
 	nodeData = postprocessNodes(nodeData);
 	return nodeData;
 }
-
 
 
 async function fetchData(characterAvatar) {
@@ -230,158 +230,186 @@ async function navigateToMessage(chatSessionName, messageId) {
 }
 
 
-function createDynamicContextMenu(chatSessions) {
-	let contextMenuAdornment = goMake(go.Adornment, "Vertical");
-	console.log(chatSessions);
 
-	chatSessions.forEach((chatSession, i) => {
-		contextMenuAdornment.add(
-			goMake("ContextMenuButton",
-				goMake(go.TextBlock, `${chatSession}`),
-				{
-					click: function (e, obj) {
-						navigateToMessage(chatSession, i);
-					}
-				})
-		);
-	});
-
-	return contextMenuAdornment;
+// Function to create a node template
+function createNodeTemplate(goMake) {
+    return goMake(go.Node, "Auto",
+        {
+            doubleClick: nodeClickHandler,
+            contextMenu: createContextMenuAdornment(goMake),
+        },
+        createShape(goMake),
+        { toolTip: createTooltipAdornment(goMake) }
+    );
 }
 
+// Function to handle click events on nodes
+function nodeClickHandler(e, obj) {
+    let node = obj.part;
+    let depth = getNodeDepth(node);
+    let chatSessions = obj.part.data.chat_sessions;
+    if (!(chatSessions && chatSessions.length > 1)) {
+        let chatSessionName = obj.part.data.file_name;
+        navigateToMessage(chatSessionName, depth);
+    }
+}
 
+// Function to get node depth
+function getNodeDepth(node) {
+    let depth = 0;
+    while (node.findTreeParentNode() !== null) {
+        node = node.findTreeParentNode();
+        depth++;
+    }
+    return depth;
+}
 
+// Function to create a context menu adornment
+function createContextMenuAdornment(goMake) {
+    return goMake(go.Adornment, "Vertical",
+        new go.Binding("itemArray", "chat_sessions"), 
+        { itemTemplate: createContextMenuButton(goMake) }
+    );
+}
 
+// Function to create a context menu button
+function createContextMenuButton(goMake) {
+    return goMake("ContextMenuButton",
+        goMake(go.TextBlock, new go.Binding("text", "")), 
+        { click: contextMenuButtonClickHandler }
+    );
+}
 
+// Function to handle click events on context menu buttons
+function contextMenuButtonClickHandler(e, button) {
+    let chatSession = button.part.data; 
+    let node = e.diagram.findNodeForData(button.part.adornedPart.data);
+    let depth = getNodeDepth(node);
+    navigateToMessage(chatSession.file_name, depth);
+}
 
+// Function to create a shape
+function createShape(goMake) {
+    return goMake(go.Shape, "Circle",
+        { width: 25, height: 25 },
+        new go.Binding("fill", "is_user", (is_user) => is_user ? "lightblue" : "white"),
+        new go.Binding("stroke", "isBookmark", (isBookmark) => isBookmark ? "gold" : null),
+        new go.Binding("strokeWidth", "isBookmark", (isBookmark) => isBookmark ? 3 : 0),
+    );
+}
+
+// Function to create a tooltip adornment
+function createTooltipAdornment(goMake) {
+    return goMake(go.Adornment, "Auto",
+        goMake(go.Shape, { fill: "#EFEFCC" }),
+        goMake(go.TextBlock, { margin: 4 },
+            new go.Binding("text", "", formatTooltipText)
+        )
+    );
+}
+
+// Function to format tooltip text
+function formatTooltipText(d) {
+    let text = `Text: ${d.text ? d.text : "N/A"}`;
+    let fileName = d.file_name ? `\nFile Name: ${d.file_name}` : "";
+    let sendDate = d.send_date ? `\nSend Date: ${d.send_date}` : "";
+    let bookmarkName = d.bookmarkName ? `\nBookmark Name: ${d.bookmarkName}` : "";
+    let index = d.messageIndex ? `\nIndex: ${d.messageIndex}` : "";
+    return text + fileName + sendDate + bookmarkName + index;
+}
+
+// Function to highlight path to root
+function highlightPathToRoot(myDiagram, node, currentHighlightThickness = 4) {
+    if (node === null) return;
+    let color = node.data.color;
+    let parentNode = node;
+    while (parentNode !== null) {
+        let link = parentNode.findLinksInto();
+        while (link.next()) {
+            myDiagram.model.set(link.value.data, "isHighlight", true);
+            myDiagram.model.set(link.value.data, "color", color);
+            myDiagram.model.set(link.value.data, "highlightThickness", currentHighlightThickness);
+        }
+        parentNode = parentNode.findTreeParentNode();
+    }
+    currentHighlightThickness = Math.min(currentHighlightThickness + 0.1, 6); 
+}
+
+// Main renderTreeDiagram function
 function renderTreeDiagram(nodeData) {
-	console.log(nodeData);
-	let myDiagramDiv = document.getElementById('myDiagramDiv');
-	if (!myDiagramDiv) {
-		console.error('Unable to find element with id "myDiagramDiv". Please ensure the element exists at the time of calling this function.');
-		return;
-	}
-	//dynamic sizing
-	myDiagramDiv.style.width = "1600px";  // Set appropriate size
-	myDiagramDiv.style.height = "1200px"; // Set appropriate size
+    console.log(nodeData);
+    let myDiagramDiv = document.getElementById('myDiagramDiv');
+    if (!myDiagramDiv) {
+        console.error('Unable to find element with id "myDiagramDiv". Please ensure the element exists at the time of calling this function.');
+        return;
+    }
+	// Set the dimensions of the diagram to fill its parent element
+	myDiagramDiv.style.width = "100%";
+	myDiagramDiv.style.height = "100%";
+
+	let goMake = go.GraphObject.make;
 	let myDiagram = goMake(go.Diagram, myDiagramDiv, {
 		"undoManager.isEnabled": true,
 		allowMove: true,
 		allowCopy: false,
 		allowDelete: false,
 		allowInsert: false,
+		allowZoom: true,
+		// Adjusting diagram's initial stretch to fill its container
+		initialAutoScale: go.Diagram.Uniform,
+		// Adjusting diagram's auto scale to resize when its container's size changes
+		//autoScale: go.Diagram.Uniform
 	});
 
-	myDiagram.nodeTemplate =
-		goMake(go.Node, "Auto",
-			{
-				click: function (e, obj) {
-					let node = obj.part;
-					let depth = 0;
-					while (node.findTreeParentNode() !== null) {
-						node = node.findTreeParentNode();
-						depth++;
-					}
+    myDiagram.nodeTemplate = createNodeTemplate(goMake);
+    myDiagram.linkTemplate = createLinkTemplate(goMake);
 
-					let chatSessions = obj.part.data.chat_sessions;
-					if (!(chatSessions && chatSessions.length > 1)) {
-						let chatSessionName = obj.part.data.file_name;
-						navigateToMessage(chatSessionName, depth);
-					}
-				}
-			},
-			{
-				contextMenu: // Adornment that contains context menu buttons
-					goMake(go.Adornment, "Vertical",
-						new go.Binding("itemArray", "chat_sessions"), // Bind the "chat_sessions" array to itemArray
-						{
-							itemTemplate: // Template for each context menu item
-								goMake("ContextMenuButton",
-									goMake(go.TextBlock, new go.Binding("text", "")), // Bind the chat session to the button's text
-									{
-										click: function (e, button) { // Click event handler
-											let chatSession = button.part.data; // Get the chat session data from the clicked button
-											let node = e.diagram.findNodeForData(button.part.adornedPart.data);
-											let depth = 0;
-											while (node.findTreeParentNode() !== null) {
-												node = node.findTreeParentNode();
-												depth++;
-											}
-											navigateToMessage(chatSession.file_name, depth);
-										}
-									}
-								)
-						}
-					)
-			},
-			goMake(go.Shape, "Circle",
-				{ width: 25, height: 25 },
-				new go.Binding("fill", "is_user", function (is_user) {
-					return is_user ? "lightblue" : "white";
+    let model = goMake(go.TreeModel);
+    model.nodeDataArray = nodeData;
+    myDiagram.model = model;
 
-				}),
-				new go.Binding("stroke", "isBookmark", function (isBookmark) {
-					return isBookmark ? "gold" : null;
-				}),
-				new go.Binding("strokeWidth", "isBookmark", function (isBookmark) {
-					return isBookmark ? 3 : 0;
-				}),
-			),
+    nodeData.forEach(node => {
+        if (node.isBookmark) {
+            let nodeInDiagram = myDiagram.findNodeForKey(node.key);
+            highlightPathToRoot(myDiagram, nodeInDiagram);
+        }
+    });
 
-			{  // Tooltip Adornment
-				toolTip:
-					goMake(go.Adornment, "Auto",
-						goMake(go.Shape, { fill: "#EFEFCC" }),
-						goMake(go.TextBlock, { margin: 4 },
-							new go.Binding("text", "", function (d) {
-								let text = `Text: ${d.text ? d.text : "N/A"}`;
-								let fileName = d.file_name ? `\nFile Name: ${d.file_name}` : "";
-								let sendDate = d.send_date ? `\nSend Date: ${d.send_date}` : "";
-								let bookmarkName = d.bookmarkName ? `\nBookmark Name: ${d.bookmarkName}` : "";
-								let index = d.messageIndex ? `\nIndex: ${d.messageIndex}` : "";
-								return text + fileName + sendDate + bookmarkName + index;
-							}))
-					)
-			},
-		);
+    myDiagram.layout = createLayout(goMake);
 
-	let model = goMake(go.TreeModel);
-	model.nodeDataArray = nodeData;
-	myDiagram.model = model;
+    const toggleButton = document.getElementById('toggleButton');
+    if (toggleButton) {
+        toggleButton.addEventListener('click', function () {
+            toggleTreeDirection(myDiagram);
+        });
+    }
 
-	// Highlight paths from bookmark nodes to the root
-	nodeData.forEach(node => {
-		if (node.isBookmark) {
-			let nodeInDiagram = myDiagram.findNodeForKey(node.key);
-			highlightPathToRoot(nodeInDiagram);
-		}
-	});
+    // const myOverviewDiv = document.getElementById('myOverviewDiv');
+    // const myOverview = goMake(go.Overview, myOverviewDiv);
+    // myOverview.observed = myDiagram; 
+}
 
-	myDiagram.layout = goMake(go.TreeLayout, {
-		angle: 90,  // angle to make the tree grow upwards
-		layerSpacing: 35  // you can adjust the spacing to your preference
-	});
-	// in your myDiagram.linkTemplate
-	myDiagram.linkTemplate = goMake(go.Link,
-		{ routing: go.Link.Orthogonal, corner: 5 },
-		goMake(go.Shape,
-			{ strokeWidth: 3 },
-			new go.Binding("stroke", "", function (data) { return data.isHighlight ? data.color : "#555"; }),
-			new go.Binding("strokeWidth", "highlightThickness", function (h) { return h ? h : 3; })
-		)
-	);
+// Function to create layout
+function createLayout(goMake) {
+    return goMake(go.TreeLayout, {
+        angle: 90,
+        layerSpacing: 35
+    });
+}
 
-	const toggleButton = document.getElementById('toggleButton');
-	if (toggleButton) {
-		toggleButton.addEventListener('click', function () {
-			toggleTreeDirection(myDiagram);
-		});
-	}
+// Function to create a link template
+function createLinkTemplate(goMake) {
+    return goMake(go.Link,
+        { routing: go.Link.Orthogonal, corner: 5 },
+        goMake(go.Shape,
+            { strokeWidth: 3 },
+            new go.Binding("stroke", "", (data) => data.isHighlight ? data.color : "#555"),
+            new go.Binding("strokeWidth", "highlightThickness", (h) => h ? h : 3)
+        )
+    );
+}
 
-	const myOverviewDiv = document.getElementById('myOverviewDiv');
-	const myOverview = goMake(go.Overview, myOverviewDiv);
-	myOverview.observed = myDiagram; // set the diagram for the overview to be myDiagram
-
+// Function to toggle tree direction
 function toggleTreeDirection(diagram) {
     const layout = diagram.layout;
     if (!layout) {
@@ -389,27 +417,6 @@ function toggleTreeDirection(diagram) {
         return;
     }
     layout.angle = layout.angle === 0 ? 90 : 0;
-}
-
-
-	// in your highlightPathToRoot function
-	function highlightPathToRoot(node) {
-		let currentHighlightThickness = 4; // start from 3.1
-		if (node === null) return;
-		let color = node.data.color;
-		let parentNode = node;
-		while (parentNode !== null) {
-			let link = parentNode.findLinksInto();
-			while (link.next()) {
-				// Use Model.set method to update link data
-				myDiagram.model.set(link.value.data, "isHighlight", true);
-				myDiagram.model.set(link.value.data, "color", color);
-				myDiagram.model.set(link.value.data, "highlightThickness", currentHighlightThickness);
-			}
-			parentNode = parentNode.findTreeParentNode();
-		}
-		currentHighlightThickness = Math.min(currentHighlightThickness + 0.1, 6); // increase for next highlighted path but do not exceed 4
-	}
 }
 
 
@@ -439,7 +446,7 @@ async function onTreeButtonClick() {
 // This function is called when the extension is loaded
 jQuery(async () => {
   // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+  const settingsHtml = await $.get(`${extensionFolderPath}/tree.html`);
 
   // Append settingsHtml to extensions_settings
   // extension_settings and extensions_settings2 are the left and right columns of the settings menu
