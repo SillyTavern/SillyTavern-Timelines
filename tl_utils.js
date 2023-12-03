faef7dd929b80eecfe23a110c57b58619c275213
Cohee <18619528+Cohee1207@users.noreply.github.com>
2023-12-03 15:11:41 +0200

    Fix ESLint

--- tl_utils.js
+++ tl_utils.js
@@ -1,9 +1,9 @@
-import { openCharacterChat, addOneMessage, event_types, eventSource, } from "../../../../script.js";
-import { power_user } from "../../../power-user.js";
-import { createBranch } from "../../../bookmarks.js"
-import { getTokenCount } from "../../../tokenizers.js";
-import { getContext } from "../../../extensions.js";
-import { debounce } from "../../../utils.js";
+import { openCharacterChat, addOneMessage, event_types, eventSource } from '../../../../script.js';
+import { power_user } from '../../../power-user.js';
+import { createBranch } from '../../../bookmarks.js';
+import { getTokenCount } from '../../../tokenizers.js';
+import { getContext } from '../../../extensions.js';
+import { debounce } from '../../../utils.js';
 
 
 const saveChatDebounced = debounce(() => getContext().saveChat(), 2000);
@@ -11,18 +11,18 @@ const saveChatDebounced = debounce(() => getContext().saveChat(), 2000);
 
 /**
  * Navigates to a specific chat message in a chat session by adjusting the scroll position.
- * If the message is not initially visible, the function attempts to reveal it either by 
+ * If the message is not initially visible, the function attempts to reveal it either by
  * showing hidden messages or by triggering a button to load more messages.
- * Optionally, the function can create a new branch based on the message or navigate to 
+ * Optionally, the function can create a new branch based on the message or navigate to
  * a specific swipe associated with the message.
- * 
+ *
  * @param {string} chatSessionName - Name of the chat session file (can include .jsonl extension).
  * @param {number} messageId - ID of the message to navigate to.
- * @param {number} [swipeId=-1] - Optional ID of a swipe associated with the message. If provided and >= 0, 
+ * @param {number} [swipeId=-1] - Optional ID of a swipe associated with the message. If provided and >= 0,
  *                                the function navigates to the swipe after potentially creating a new branch.
  * @param {boolean} [branch=false] - If true, creates a new branch based on the message and navigates to it.
  * @returns {Promise<void>} Resolves once the navigation is complete.
- * 
+ *
  * Behavior:
  * 1. Opens the specified chat session.
  * 2. Attempts to find and scroll to the message with the given ID.
@@ -36,18 +36,18 @@ export async function navigateToMessage(chatSessionName, messageId, swipeId=-1, 
     // Remove extension from file name
     chatSessionName = chatSessionName.replace('.jsonl', '');
     await openCharacterChat(chatSessionName);
-    
+
 
     let message = $(`div[mesid=${messageId - 1}]`); // Select the message div by the messageId
-    let chat = $("#chat");
+    let chat = $('#chat');
 
     // Check if the message is not visible if not, check for show_more_messages button
     while (!message.is(':visible')) {
 
-        console.log(`Message with id "${messageId}" is not visible.`)
+        console.log(`Message with id "${messageId}" is not visible.`);
         // Show hidden messages if they exist
         if (chat.children('.mes').not(':visible').length > 0) {
-            console.log(`Showing hidden messages.`)
+            console.log('Showing hidden messages.');
             const prevHeight = chat.prop('scrollHeight');
             chat.children('.mes').not(':visible').slice(-power_user.lazy_load).show();
             const newHeight = chat.prop('scrollHeight');
@@ -59,7 +59,7 @@ export async function navigateToMessage(chatSessionName, messageId, swipeId=-1, 
             // If no hidden messages exist, check for show_more_messages div anywhere in the page
             const showMoreBtn = $('#show_more_messages');
             if (showMoreBtn.length) {
-                console.log(`Showing more messages.`)
+                console.log('Showing more messages.');
                 showMoreBtn.trigger('mouseup');
                 // Re-select the message after showing more messages to see if it's now visible
                 message = $(`div[mesid=${messageId - 1}]`);
@@ -75,14 +75,14 @@ export async function navigateToMessage(chatSessionName, messageId, swipeId=-1, 
         let name = await createBranch(messageId-1);
         await openCharacterChat(name);
         closeOpenDrawers();
-        return
+        return;
     }
     if (swipeId >= 0) {
         let name = await createBranch(messageId-1);
         await openCharacterChat(name);
         goToSwipe(swipeId, messageId-1);
         closeOpenDrawers();
-        return
+        return;
     }
     // If message is visible, adjust the scroll position to it
     if (message.length) {
@@ -102,7 +102,7 @@ export async function navigateToMessage(chatSessionName, messageId, swipeId=-1, 
 export function closeOpenDrawers() {
     var openDrawers = $('.openDrawer').not('.pinnedOpen');
 
-    openDrawers.addClass('resizing').slideToggle(200, "swing", function () {
+    openDrawers.addClass('resizing').slideToggle(200, 'swing', function () {
         $(this).closest('.drawer-content').removeClass('resizing');
     });
 
@@ -115,7 +115,7 @@ export function closeOpenDrawers() {
  * It ensures the modal is returned to its original position in the DOM when closed.
  */
 export function closeModal() {
-    let modal = document.getElementById("myModal");
+    let modal = document.getElementById('myModal');
 
     if (!modal) {
         console.error('Modal not found!');
@@ -124,7 +124,7 @@ export function closeModal() {
 
     // Append the modal back to its original parent when closed
     document.querySelector('.timeline-view-settings_block').appendChild(modal);
-    modal.style.display = "none";
+    modal.style.display = 'none';
 }
 
 /**
@@ -134,7 +134,7 @@ export function closeModal() {
  * - Closes the modal either by clicking its close button or clicking outside of it.
  */
 export function handleModalDisplay() {
-    let modal = document.getElementById("myModal");
+    let modal = document.getElementById('myModal');
 
     // Ensure that modal exists
     if (!modal) {
@@ -142,14 +142,14 @@ export function handleModalDisplay() {
         return;
     }
 
-    let closeBtn = modal.getElementsByClassName("close")[0];
+    let closeBtn = modal.getElementsByClassName('close')[0];
 
     // Ensure that close button exists
     if (!closeBtn) {
         console.error('Close button not found!');
         return;
     }
-    
+
 
     function closeTippy() {
         // If Tippy uses a specific class or attribute, you can target it more precisely
@@ -167,34 +167,34 @@ export function handleModalDisplay() {
     closeBtn.onclick = function () {
         // Append the modal back to its original parent when closed
         document.querySelector('.timeline-view-settings_block').appendChild(modal);
-        modal.style.display = "none";
+        modal.style.display = 'none';
         closeTippy();  // Hide the Tippy tooltip
-    }
+    };
 
     window.onclick = function (event) {
         if (event.target == modal) {
             // Append the modal back to its original parent when clicked outside
             document.querySelector('.timeline-view-settings_block').appendChild(modal);
-            modal.style.display = "none";
+            modal.style.display = 'none';
             closeTippy();  // Hide the Tippy tooltip
         }
-    }
+    };
 
     // Append the modal to the body when showing it
     document.body.appendChild(modal);
-    modal.style.display = "block";
+    modal.style.display = 'block';
 }
 
 /**
  * Navigates to a specific swipe within a chat based on the given swipe ID and updates the chat data and UI accordingly.
- * 
+ *
  * This function adjusts the chat data to reflect the content of the specified swipe and updates the UI to display it.
  * It also handles edge cases such as swipe ID bounds and potential cleanup of extra properties.
- * 
+ *
  * @param {number} targetSwipeId - The ID of the swipe to navigate to.
  * @param {number} message_id - The ID of the message associated with the swipe.
  * @returns {Promise<void>} Resolves once the swipe navigation and associated updates are complete.
- * 
+ *
  * Behavior:
  * 1. Sets the desired swipe ID and validates its bounds.
  * 2. Adjusts the chat data to reflect the content of the specified swipe.
@@ -205,7 +205,7 @@ export function handleModalDisplay() {
 
 async function goToSwipe(targetSwipeId, message_id) {
     let chat = getContext().chat;
- 
+
     // Set the desired swipe ID
     chat[chat.length - 1]['swipe_id'] = targetSwipeId;
 
@@ -238,7 +238,7 @@ async function goToSwipe(targetSwipeId, message_id) {
 
     // Update token count if enabled
     if (power_user.message_token_count_enabled) {
-        const swipeMessage = $("#chat").find(`[mesid="${message_id}"]`);
+        const swipeMessage = $('#chat').find(`[mesid="${message_id}"]`);
         const tokenCount = getTokenCount(chat[chat.length - 1].mes, 0);
         chat[chat.length - 1]['extra']['token_count'] = tokenCount;
         swipeMessage.find('.tokenCounterDisplay').text(`${tokenCount}t`);
